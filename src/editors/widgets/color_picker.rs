c8326be77cb52a7f843cd477195215740961ada9
Ricardo Massaro <ricardo.massaro@gmail.com>
2026-04-26 01:25:48 -0300

    Improve color picker

--- src/editors/widgets/color_picker.rs
+++ src/editors/widgets/color_picker.rs
@@ -2,8 +2,8 @@ use std::sync;
 use egui::{Vec2, Sense, Rect, Pos2};
 
 use crate::app::WindowContext;
-use crate::misc::colors::raven_color_to_rgb;
 
+const CLOSE_PICKER_ON_CLICK: bool = false;
 const MIN_PICKER_WIDTH: f32 = 112.0;
 const COLOR_BLOCK_SIZE: f32 = 20.0;
 
@@ -39,36 +39,44 @@ fn create_all_pal6() -> Vec<u8> {
     Vec::from(colors)
 }
 
-const GRAY_PAL8: [u8; 6] = [
+const GRAY_PAL8: [u8; 8] = [
     0b00_000_000,
+    0b00_001_001,
     0b01_010_010,
     0b01_011_011,
     0b10_100_100,
     0b10_101_101,
+    0b11_110_110,
     0b11_111_111
 ];
 const GRAD_PAL8: [u8; 78] = [
+    // blue
     0b01_000_000, 0b01_000_000, 0b01_000_000, 0b10_000_000, 0b10_000_000, 0b10_000_000,
     0b11_000_000,
     0b11_001_001, 0b11_010_010, 0b11_011_011, 0b11_100_100, 0b11_101_101, 0b11_110_110,
 
+    // green
     0b00_001_000, 0b00_010_000, 0b00_011_000, 0b00_100_000, 0b00_101_000, 0b00_110_000,
     0b00_111_000,
     0b00_111_001, 0b00_111_010, 0b01_111_011, 0b01_111_100, 0b10_111_101, 0b10_111_110,
 
+    // red
     0b00_000_001, 0b00_000_010, 0b00_000_011, 0b00_000_100, 0b00_000_101, 0b00_000_110,
     0b00_000_111,
     0b00_001_111, 0b00_010_111, 0b01_011_111, 0b01_100_111, 0b10_101_111, 0b10_110_111,
 
-    0b00_001_000, 0b00_010_000, 0b01_011_000, 0b01_100_000, 0b10_101_000, 0b10_110_000,
+    // cyan
+    0b01_001_000, 0b01_010_000, 0b01_011_000, 0b10_100_000, 0b10_101_000, 0b10_110_000,
     0b11_111_000,
     0b11_111_001, 0b11_111_010, 0b11_111_011, 0b11_111_100, 0b11_111_101, 0b11_111_110,
 
+    // yellow
     0b00_001_001, 0b00_010_010, 0b00_011_011, 0b00_100_100, 0b00_101_101, 0b00_110_110,
     0b00_111_111,
     0b00_111_111, 0b00_111_111, 0b01_111_111, 0b01_111_111, 0b10_111_111, 0b10_111_111,
 
-    0b00_000_001, 0b00_000_010, 0b01_000_011, 0b01_000_100, 0b10_000_101, 0b10_000_110,
+    // magenta
+    0b01_000_001, 0b01_000_010, 0b01_000_011, 0b01_000_100, 0b10_000_101, 0b10_000_110,
     0b11_000_111,
     0b11_001_111, 0b11_010_111, 0b11_011_111, 0b11_100_111, 0b11_101_111, 0b11_110_111,
 ];
@@ -99,6 +107,16 @@ impl<'a> ColorPickerWidget6<'a> {
         }
     }
 
+    pub fn color_to_rgb(color: u8) -> egui::Color32 {
+        let r = (color as u8 >> 1) & 0x3;
+        let g = (color as u8 >> 4) & 0x3;
+        let b = (color as u8 >> 6) & 0x3;
+        let cr = (r << 6) | (r << 4) | (r << 2) | r;
+        let cg = (g << 6) | (g << 4) | (g << 2) | g;
+        let cb = (b << 6) | (b << 4) | (b << 2) | b;
+        egui::Color32::from_rgb(cr, cg, cb)
+    }
+
     fn draw_palette(painter: &egui::Painter, rect: Rect, dims: (i32, i32), palette: &[u8]) {
         let item_w = rect.width() / (dims.0 as f32);
         let item_h = rect.height() / (dims.1 as f32);
@@ -109,7 +127,7 @@ impl<'a> ColorPickerWidget6<'a> {
                     max: Pos2::new(rect.min.x + ((x+1) as f32) * item_w, rect.min.y + ((y+1) as f32) * item_h),
                 };
                 let color_index = (y*dims.0+x) as usize;
-                painter.rect_filled(item_rect, egui::CornerRadius::ZERO, raven_color_to_rgb(palette[color_index]));
+                painter.rect_filled(item_rect, egui::CornerRadius::ZERO, Self::color_to_rgb(palette[color_index]));
             }
         }
     }
@@ -223,7 +241,29 @@ impl<'a> ColorPickerWidget8<'a> {
         }
     }
 
-    fn draw_palette(painter: &egui::Painter, rect: Rect, dims: (i32, i32), palette: &[u8]) {
+    fn get_contrast_color(color: u8) -> egui::Color32 {
+        let r = (color & 0x7) as f32;
+        let g = ((color >> 3) & 0x7) as f32;
+        let b = (((color >> 5) & 0x6) | (color >> 7)) as f32;
+        let brightness = r*0.3 + g*0.8 + b*0.1;
+        if brightness < 4.5 {
+            egui::Color32::WHITE
+        } else {
+            egui::Color32::BLACK
+        }
+    }
+
+    pub fn color_to_rgb(color: u8) -> egui::Color32 {
+        let r = color & 0x7;
+        let g = (color >> 3) & 0x7;
+        let b = (color >> 6) & 0x3;
+        let cr = (r << 5) | (r << 2) | (r >> 2);
+        let cg = (g << 5) | (g << 2) | (g >> 2);
+        let cb = (b << 6) | (b << 4) | (b << 2) | b;
+        egui::Color32::from_rgb(cr, cg, cb)
+    }
+
+    fn draw_palette(painter: &egui::Painter, rect: Rect, dims: (i32, i32), palette: &[u8], sel_color: Option<u8>) {
         let item_w = rect.width() / (dims.0 as f32);
         let item_h = rect.height() / (dims.1 as f32);
         for y in 0..dims.1 {
@@ -232,8 +272,12 @@ impl<'a> ColorPickerWidget8<'a> {
                     min: Pos2::new(rect.min.x + (x     as f32) * item_w, rect.min.y + (y     as f32) * item_h),
                     max: Pos2::new(rect.min.x + ((x+1) as f32) * item_w, rect.min.y + ((y+1) as f32) * item_h),
                 };
-                let color_index = (y*dims.0+x) as usize;
-                painter.rect_filled(item_rect, egui::CornerRadius::ZERO, raven_color_to_rgb(palette[color_index]));
+                let color = palette[(y*dims.0+x) as usize];
+                painter.rect_filled(item_rect, egui::CornerRadius::ZERO, Self::color_to_rgb(color));
+                if let Some(sel_color) = sel_color && sel_color == color {
+                    let stroke = egui::Stroke::new(1.0, Self::get_contrast_color(color));
+                    painter.rect_stroke(item_rect, egui::CornerRadius::ZERO, stroke, egui::StrokeKind::Inside);
+                }
             }
         }
     }
@@ -246,7 +290,7 @@ impl<'a> ColorPickerWidget8<'a> {
             .show(ui, |ui| {
                 let min_size = Vec2::new(MIN_PICKER_WIDTH - 16.0, 24.0);
                 let (response, painter) = ui.allocate_painter(min_size, Sense::click());
-                Self::draw_palette(&painter, response.rect, (2, 1), &[self.state.left_color, self.state.right_color]);
+                Self::draw_palette(&painter, response.rect, (2, 1), &[self.state.left_color, self.state.right_color], None);
 
                 if response.clicked() && let Some(pos) = response.interact_pointer_pos() {
                     self.state.open_color = Some(if pos.x <= response.rect.center().x { OpenColor::Left } else { OpenColor::Right });
@@ -274,7 +318,7 @@ impl<'a> ColorPickerWidget8<'a> {
                         ui.horizontal(|ui| {
                             let size = Vec2::new(32.0 * COLOR_BLOCK_SIZE, 8.0 * COLOR_BLOCK_SIZE);
                             let (response, painter) = ui.allocate_painter(size, Sense::click_and_drag());
-                            Self::draw_palette(&painter, response.rect, (32, 8), &ALL_PAL8);
+                            Self::draw_palette(&painter, response.rect, (32, 8), &ALL_PAL8, Some(self.get_open_color()));
                             if let Some(pos) = response.interact_pointer_pos() && response.rect.contains(pos) {
                                 let x = ((pos.x - response.rect.min.x) / COLOR_BLOCK_SIZE).floor() as i32;
                                 let y = ((pos.y - response.rect.min.y) / COLOR_BLOCK_SIZE).floor() as i32;
@@ -283,7 +327,7 @@ impl<'a> ColorPickerWidget8<'a> {
                                     self.set_open_color(ALL_PAL8[index as usize]);
                                 }
                             }
-                            if response.clicked() {
+                            if (CLOSE_PICKER_ON_CLICK && response.clicked()) || response.double_clicked() {
                                 self.state.open_color = None;
                             }
                         });
@@ -298,7 +342,7 @@ impl<'a> ColorPickerWidget8<'a> {
                                 // grays palette
                                 ui.horizontal(|ui| {
                                     let num_colors = GRAY_PAL8.len() as i32;
-                                    let h_color_block_size = COLOR_BLOCK_SIZE * 13.0 / 6.0;
+                                    let h_color_block_size = COLOR_BLOCK_SIZE * 13.0 / (num_colors as f32);
                                     let alloc_size = Vec2::new(32.0 * COLOR_BLOCK_SIZE - 6.0, COLOR_BLOCK_SIZE);
                                     let (response, painter) = ui.allocate_painter(alloc_size, Sense::click_and_drag());
                                     let pal_width = h_color_block_size * num_colors as f32;
@@ -306,30 +350,30 @@ impl<'a> ColorPickerWidget8<'a> {
                                         min: egui::Pos2::new(response.rect.center().x - pal_width/2.0, response.rect.min.y),
                                         max: egui::Pos2::new(response.rect.center().x + pal_width/2.0, response.rect.max.y),
                                     };
-                                    Self::draw_palette(&painter, draw_rect, (num_colors, 1), &GRAY_PAL8);
+                                    Self::draw_palette(&painter, draw_rect, (num_colors, 1), &GRAY_PAL8, Some(self.get_open_color()));
                                     if let Some(pos) = response.interact_pointer_pos() && response.rect.contains(pos) {
                                         let index = ((pos.x - draw_rect.min.x) / h_color_block_size).floor() as i32;
                                         if index >= 0 && index < num_colors {
                                             self.set_open_color(GRAY_PAL8[index as usize]);
                                         }
                                     }
-                                    if response.clicked() {
+                                    if (CLOSE_PICKER_ON_CLICK && response.clicked()) || response.double_clicked() {
                                         self.state.open_color = None;
                                     }
                                 });
 
-                                ui.add_space(8.0);
+                                ui.add_space(2.0);
 
                                 // gradients palette
                                 ui.horizontal(|ui| {
-                                    let alloc_size = Vec2::new(32.0 * COLOR_BLOCK_SIZE - 5.0, 6.0 * COLOR_BLOCK_SIZE);
+                                    let alloc_size = Vec2::new(32.0 * COLOR_BLOCK_SIZE - 6.0, 6.0 * COLOR_BLOCK_SIZE);
                                     let (response, painter) = ui.allocate_painter(alloc_size, Sense::click_and_drag());
                                     let pal_width = COLOR_BLOCK_SIZE * 13.0;
                                     let draw_rect = egui::Rect {
                                         min: egui::Pos2::new(response.rect.center().x - pal_width/2.0, response.rect.min.y),
                                         max: egui::Pos2::new(response.rect.center().x + pal_width/2.0, response.rect.max.y),
                                     };
-                                    Self::draw_palette(&painter, draw_rect, (13, 6), &GRAD_PAL8);
+                                    Self::draw_palette(&painter, draw_rect, (13, 6), &GRAD_PAL8, Some(self.get_open_color()));
                                     if let Some(pos) = response.interact_pointer_pos() && response.rect.contains(pos) {
                                         let x = ((pos.x - draw_rect.min.x) / COLOR_BLOCK_SIZE).floor() as i32;
                                         let y = ((pos.y - draw_rect.min.y) / COLOR_BLOCK_SIZE).floor() as i32;
@@ -338,7 +382,7 @@ impl<'a> ColorPickerWidget8<'a> {
                                             self.set_open_color(GRAD_PAL8[index as usize]);
                                         }
                                     }
-                                    if response.clicked() {
+                                    if (CLOSE_PICKER_ON_CLICK && response.clicked()) || response.double_clicked() {
                                         self.state.open_color = None;
                                     }
                                 });
--- src/misc/colors.rs
+++ src/misc/colors.rs
@@ -2,13 +2,3 @@
 pub const RED   : u8 = 0b00000111;
 pub const GREEN : u8 = 0b00111000;
 pub const BLUE  : u8 = 0b11000000;
-
-pub fn raven_color_to_rgb(raven_color: u8) -> egui::Color32 {
-    let r = raven_color & 0x7;
-    let g = (raven_color >> 3) & 0x7;
-    let b = (raven_color >> 6) & 0x3;
-    let cr = (r << 5) | (r << 2) | (r >> 2);
-    let cg = (g << 5) | (g << 2) | (g >> 2);
-    let cb = (b << 6) | (b << 4) | (b << 2) | b;
-    egui::Color32::from_rgb(cr, cg, cb)
-}
