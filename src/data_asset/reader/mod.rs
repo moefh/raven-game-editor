b5d377b6ceaa71964d3d7a49076ce0b68204017b
Ricardo Massaro <ricardo.massaro@gmail.com>
2026-05-30 02:12:31 -0300

    Properly ignore pal_sprite #defines when reading project

--- src/data_asset/reader/mod.rs
+++ src/data_asset/reader/mod.rs
@@ -389,6 +389,12 @@ impl<'a> ProjectDataReader<'a> {
             name_id.starts_with(&format!("{}SPRITE_FRAMES_", self.prefix_upper)) {
                 return;
             }
+        if name_id.starts_with(&format!("{}PAL_SPRITE_WIDTH_", self.prefix_upper)) ||
+            name_id.starts_with(&format!("{}PAL_SPRITE_HEIGHT_", self.prefix_upper)) ||
+            name_id.starts_with(&format!("{}PAL_SPRITE_FRAMES_", self.prefix_upper)) ||
+            name_id.starts_with(&format!("{}PAL_SPRITE_DEPTH_", self.prefix_upper)) {
+                return;
+            }
         self.logger.log(format!("-> ignoring define '{}' = '{}'", name_id, value));
     }
 
